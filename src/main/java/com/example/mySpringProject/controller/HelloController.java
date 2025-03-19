e48bd9f3d2df99fbb705a36745df92fcee69b2ca
Hiram Pina <hiram.p@gmail.com>
2025-03-19 00:26:27 -0400

    Add endpoint to display local date and time

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.mySpringProject.controller;
 
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,11 @@ public class HelloController {
   @GetMapping("/helloworld")
   public String helloWorld() {
     return "Hello World!";
+  }
+
+  @GetMapping("/datetime")
+  public String localDateTime() {
+    LocalDateTime dateTime = new LocalDateTime();
+    return dateTime.toString();
   }
 }
